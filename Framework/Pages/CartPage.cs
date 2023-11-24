ef05748c93ebea157fb300b447fc8ad7053efeb7
MartynasVCS <martynasvcs@gmail.com>
2023-11-24 12:28:20 +0200

    Remove escape characters from xpaths

--- Framework/Pages/CartPage.cs
+++ Framework/Pages/CartPage.cs
@@ -4,7 +4,7 @@
     {
         public static string GetItemAddedToShoppingCart()
         {
-            string locator = "//*[@id=\"cart_items\"]/table/tbody/tr[1]/td[2]/a[1]";
+            string locator = "//*[@id='cart_items']/table/tbody/tr[1]/td[2]/a[1]";
             return Common.GetText(locator);
         }
     }
--- Framework/Pages/HomePage.cs
+++ Framework/Pages/HomePage.cs
@@ -9,13 +9,13 @@
 
         public static void ClickButtonDeclineCookies()
         {
-            string locator = "//*[@id=\"CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll\"]";
+            string locator = "//*[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']";
             Common.ClickElement(locator);
         }
 
         public static void ClickOnItemInHomePage()
         {
-            string locator = "//*[@id=\"icons_149000518934\"]/div[1]/div/div[1]/div/a/img";
+            string locator = "//*[@id='icons_149000518934']/div[1]/div/div[1]/div/a/img";
             Common.ClickElement(locator);
         }
 
@@ -23,13 +23,13 @@
         {
             public static void EnterSearchText(string text)
             {
-                string locator = "//*[@id=\"search_input\"]";
+                string locator = "//*[@id='search_input']";
                 Common.SendKeysToElement(locator, text);
             }
 
             public static void ClickButtonSearch()
             {
-                string locator = "//*[@class=\"ty-search-magnifier\"]";
+                string locator = "//*[@class='ty-search-magnifier']";
                 Common.ClickElement(locator);
             }
         }
@@ -38,13 +38,13 @@
         {
             public static void ClickOpenShoppingCart()
             {
-                string locator = "//*[@id=\"dropdown_166\"]/div/div[3]/div[1]/a";
+                string locator = "//*[@id='dropdown_166']/div/div[3]/div[1]/a";
                 Common.ClickElement(locator);
             }
 
             public static void ClickOpenShoppingCartDropdown()
             {
-                string locator = "//*[@id=\"sw_dropdown_166\"]";
+                string locator = "//*[@id='sw_dropdown_166']";
                 Common.ClickElement(locator);
             }
         }
